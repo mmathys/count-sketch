839fd095faad19896d9d26d944012bfa32b35134
Max Mathys <max.mathys@gmail.com>
2022-03-08 14:24:27 +0100

    increased hyperparams

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from CountSketch.CountSketch import CountSketch
 import random
 import numpy as np
 
-cs = CountSketch(4, 4)
+cs = CountSketch(100, 100)
 s = np.random.normal(10, 1, 1000)
 rng = [0, 20]
 
@@ -11,6 +11,10 @@ for num in s:
     if num in range(rng[0], rng[1]):
         cs.add(num.to_bytes(2, "big"))
 
+estimates = []
 for i in range(rng[0], rng[1]):
     q = i.to_bytes(2, "big")
-    print(f"{i}: {cs.estimate(q)}")
+    estimates.append(cs.estimate(q))
+
+print(f"sum: {np.sum(estimates)}")
+print(estimates)
